e1356bb8ae714dd45eb5f6c77d0150f0deb664ad
cloudymax <emax@cloudydev.net>
2022-12-18 08:35:31 +0000

    moving this into its own repo

---
+++ azure-network.tf
@@ -0,0 +1,26 @@
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "vm${var.vnet_subnet_name}"
+  resource_group_name  = var.resource_group
+  virtual_network_name = "${var.environment}-${var.vnet_name}"
+  address_prefixes     = var.subnet_prefixes
+}
+
+resource "azurerm_network_security_group" "vm_security_group" {
+  name                = "VmNetworkSecurityGroup"
+  location            = var.location
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_network_security_rule" "example" {
+  name                        = "SSH"
+  priority                    = 1001 
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22" 
+  source_address_prefixes     = var.allowed_ips
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.vm_security_group.name
+}
