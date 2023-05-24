0cf09c0a804ec97602a65d8ce3d85778495afce0
Max! <emax@cloudydev.net>
2023-05-24 20:30:59 +0200

    Update azure-network.tf

--- azure-network.tf
+++ azure-network.tf
@@ -21,7 +21,7 @@ resource "azurerm_network_security_rule" "ssh" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefixes     = var.allowed_ips
+  source_address_prefixes     = ["${var.allowed_ips}"]
   destination_address_prefix  = "*"
   resource_group_name         = var.resource_group
   network_security_group_name = azurerm_network_security_group.scaleset_security_group.name
