da4090ca0a9f1744bb2074acd655a89193b5016c
Max! <emax@cloudydev.net>
2023-09-19 22:10:31 +0200

    Update azure-network.tf

--- azure-network.tf
+++ azure-network.tf
@@ -7,22 +7,120 @@ resource "azurerm_subnet" "vm_subnet" {
   service_endpoints    = [ "Microsoft.Storage" ]
 }
 
-resource "azurerm_network_security_group" "scaleset_security_group" {
+resource "azurerm_network_security_group" "security_group" {
   name                = "ScaleSet-NetSec"
   location            = var.location
   resource_group_name = var.resource_group
 }
 
 resource "azurerm_network_security_rule" "ssh" {
-  name                        = "SSH"
-  priority                    = 1001
+  name                        = "ssh"
+  priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefixes     = var.allowed_ips
+  source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = var.resource_group
-  network_security_group_name = azurerm_network_security_group.scaleset_security_group.name
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "https" {
+  name                        = "https"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "vnc" {
+  name                        = "vnc"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5900"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "novnc" {
+  name                        = "novnc"
+  priority                    = 130
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "sunshine" {
+  name                        = "sunshine"
+  priority                    = 140
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "48010"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "sunshine-tcp" {
+  name                        = "sunshine-tcp"
+  priority                    = 150
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "47984-47990"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "sunshine-udp" {
+  name                        = "sunshine-udp"
+  priority                    = 160
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  source_port_range           = "*"
+  destination_port_range      = "47998-48000"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
+}
+
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "rdp"
+  priority                    = 170
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group
+  network_security_group_name = azurerm_network_security_group.security_group.name
 }
